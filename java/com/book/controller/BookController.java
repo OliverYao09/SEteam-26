88b77d35f69f76f045d9ffc6cd2a07ffb2d1fb76
nvd4312 <71802550+nvd4312@users.noreply.github.com>
2020-10-15 13:47:55 +0800

    Add files via upload

---
+++ java/com/book/controller/BookController.java
@@ -0,0 +1,86 @@
+package com.book.controller;
+
+import com.book.entity.Book;
+import com.book.service.BookService;
+import com.sun.xml.internal.bind.v2.util.CollisionCheckStack;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * (Book)Control Table
+ *
+ * @author makejava
+ * @since 2020-09-26 10:04:33
+ */
+@Controller
+@RequestMapping("book")
+public class BookController {
+    /**
+     * Service object
+     */
+    @Resource
+    private BookService bookService;
+
+    /**
+     * Query single data by primary key
+     *
+     * @param id Primary key
+     * @return Single data
+     */
+    @GetMapping("selectOne")
+    public Book selectOne(Integer id) {
+        return this.bookService.queryById(id);
+    }
+
+    /**
+     * add book
+     * @param book
+     * @return
+     */
+    @PostMapping("addBook")
+    public String addBook(Book book) {
+        Book book2 = bookService.insert(book);
+        return "submit";
+    }
+
+    /**
+     * delete book
+     * @param id
+     * @return
+     */
+    @DeleteMapping("delBook")
+    @ResponseBody
+    public String delBook(Integer id) {
+        boolean flag = bookService.deleteById(id);
+        return "";
+    }
+
+    /**
+     * according to the title, author, year or doi to search book
+     * @return
+     */
+    @GetMapping("findByCondition")
+    public String findByCondition(Map<String,Object> map,String con){
+        List<Book> books = bookService.findByCondition(con);
+        map.put("books", books);
+        return "search";
+    }
+
+    /**
+     * Multi-condition query
+     * @param map
+     * @return
+     */
+    @GetMapping("findByManyCon")
+    public String findByManyCon(Map<String,Object> map,String time1,String time2,
+                                String field,String operator,String value){
+        List<Book> books = bookService.findByManyCon(time1,time2,field,operator,value);
+        map.put("books", books);
+        return "search";
+    }
+
+}
